b02bf4431e1307fc70c29cb590a1ec74b7be0686
Tyler2k <thpearso@asu.edu>
2017-12-30 17:51:08 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+const API_KEY = 'AIzaSyC3rctA0Z7mIdvbjTex-690YMOikOhbAFQ';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos: []
+		};
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({videos: videos});  // or { videos } because key and value are same name
+		});
+
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
+}
+
+ReactDom.render(<App />, document.querySelector('.container'));
